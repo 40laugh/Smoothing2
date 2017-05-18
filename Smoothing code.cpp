6416f48d73daf678351e467c2f68a42e5e31fdbb
40laugh <ryankristek@gmail.com>
2017-05-17 21:54:38 -0500

    Update Smoothing code.cpp

--- Smoothing code.cpp
+++ Smoothing code.cpp
@@ -3,8 +3,13 @@
 // to handle negative numbers. The "u" before int means unsigned (only positive).
 #include <cstdint>
 
-#define MOV_AVG_SIZE  20                    // keep this many readings
-#define THRESHOLD     1.5                   // reading must be 1.5x higher than avg
+#define MOV_AVG_SIZE  50                    // keep this many readings
+#define THRESHOLD     0.97                  // reading must be X% lower than avg
+#define LED_PIN       LED_BUILTIN           //defining light pin
+#define LIGHT_DURATION 5                   //tenths of a second 1/10th second
+
+
+
 
 struct MovingAverage {
     std::uint16_t data[MOV_AVG_SIZE] = {0}; // hold the values here, all values default to 0
@@ -13,10 +18,10 @@ struct MovingAverage {
     std::uint16_t sum{0};                   // start with a sum of 0
 
     std::uint16_t get() {
-        if (valid_num == 0) { return 0; }   // dopn't divide by 0
+        if (valid_num == 0) { return 0; }   // don't divide by 0
         return sum / valid_num;
-    }                                       
-    
+    }
+
     void insert(std::uint16_t val) {
         sum -= data[cursor];                // remove this reading from the sum
         data[cursor] = val;                 // replace old value with new
@@ -27,37 +32,44 @@ struct MovingAverage {
         }
     }
 };                                          //End of running average
+MovingAverage avg;
 
-std::uint16_t get_reading() {
-    // this always returns 7 just because.... put your "read the pin" code here
-    return 7;
+inline std::uint16_t get_targets_pressure() {
+    return analogRead(A0);
 }
 
-void do_lights() {
-    // turn on your lights, play music, etc
+void set_lights(bool on) {
+    if(on) {
+        digitalWrite(LED_PIN, HIGH);
+    } else {
+        digitalWrite(LED_PIN, LOW);
+    }
 }
-
-int main() {
-    MovingAverage avg;
+//This is for setting up the loop of checking target, turning lights on, etc...
+void setup() {
+    Serial.begin(9600);
+    pinMode(LED_PIN,OUTPUT);
 
     // "prime" the avg
     for (int i = 0; i < 100; ++i) {         // do 100 readings to get a "reliable" starting avg
-        std::uint16_t r = get_reading();    // read the pin value
+        std::uint16_t r = get_targets_pressure();    // read the pin value
         avg.insert(r);                      // insert reading
     }
+}
 
-    // off to the races
-    for (;;) {                              // infinite loop
-        std::uint16_t r = get_reading();    // read the pin value
 
-        // check if we are over the threshold
-        // only add to the average if _not_ over the threshold otherwise the
-        // average will potentially get skewed... probably not that big of a deal
-        // but what I would suggest
-        if (r > avg.get()*THRESHOLD) {
-            do_lights();                    // we crossed the threshold, do stuff
-        } else {
-            avg.insert(r);                  // only insert reading if not across the threshold
-        }
+void loop() {
+    std::uint16_t r = get_targets_pressure();    // read the pin value
+    Serial.println(r);
+    // check if we are over the threshold
+    // only add to the average if _not_ over the threshold otherwise the
+    // average will potentially get skewed... probably not that big of a deal
+    // but what I would suggest
+    if (r < avg.get()*THRESHOLD) {
+        set_lights(true);
+        delay(5*LIGHT_DURATION);
+        set_lights(false);
+    } else {
+        avg.insert(r);                  // only insert reading if not across the threshold
     }
 }
